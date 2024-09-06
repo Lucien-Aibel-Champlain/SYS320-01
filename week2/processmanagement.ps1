ce1e77ad892b133cc0f398f8351b29288568207c
Lucien Aibel <lucien.aibel@mymail.champlain.edu>
2024-09-06 18:49:28 -0400

    Script 4 submission for Week 2

---
+++ week2/processmanagement.ps1
@@ -0,0 +1,10 @@
+﻿$chrome = Get-Process "chrome" -ea Ignore
+if ($chrome)
+{
+    Stop-Process $chrome
+}
+else
+{
+    $chrome = Start-Process "chrome" `
+        -ArgumentList "https://www.champlain.edu"
+}

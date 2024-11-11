9758a243c3b84fbf0d74d5d823ae941c7cec3b6b
Lucien's ChampUser on xubuntu <lucien.aibel@mymail.champlain.edu>
2024-11-10 19:46:46 -0500

    Week 11 done and dusted.

---
+++ week11/apacheLogMenu-1.bash
@@ -0,0 +1,103 @@
+#! /bin/bash
+
+logFile="/var/log/apache2/access.log"
+
+function displayAllLogs(){
+	cat "$logFile"
+}
+
+function displayOnlyIPs(){
+        cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
+}
+
+function displayOnlyPages(){
+        cat "$logFile" | cut -d ' ' -f 7 | sort -n | uniq -c
+}
+
+function histogram(){
+
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '['  | sort \
+                              | uniq)
+	# This is for debugging, print here to see what it does to continue:
+	# echo "$visitsPerDay"
+
+        :> newtemp.txt  # what :> does is in slides
+	echo "$visitsPerDay" | while read -r line;
+	do
+		local withoutHours=$(echo "$line" | cut -d " " -f 2 \
+                                     | cut -d ":" -f 1)
+		local IP=$(echo "$line" | cut -d  " " -f 1)
+          
+		local newLine="$IP $withoutHours"
+		echo "$IP $withoutHours" >> newtemp.txt
+	done 
+	cat "newtemp.txt" | sort -n | uniq -c
+}
+
+function frequentVisitors() {	
+	echo "$(cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c)" | while read -r line;
+	do
+		if [[ $(echo "$line" | cut -d ' ' -f 1) > 10 ]]; then	\
+			echo "$line"
+		fi
+	done
+}
+
+function suspiciousVisitors() {
+	echo "$(cat 'ioc.txt')" | while read -r line;
+	do
+		grep "$line" "$logFile"
+	done | cut -d ' ' -f 1 | sort -n | uniq -c
+}
+
+# Keep in mind that I have selected long way of doing things to 
+# demonstrate loops, functions, etc. If you can do things simpler,
+# it is welcomed.
+
+while :
+do
+	echo "Please select an option:"
+	echo "[1] Display all Logs"
+	echo "[2] Display only IPS"
+	echo "[3] Display only pages visited"
+	echo "[4] Histogram"
+	echo "[5] Frequent visitors"
+	echo "[6] Suspicious Visitors"
+	echo "[7] Quit"
+
+	read userInput
+	echo ""
+
+	if [[ "$userInput" == "7" ]]; then
+		echo "Goodbye"		
+		break
+
+	elif [[ "$userInput" == "1" ]]; then
+		echo "Displaying all logs:"
+		displayAllLogs
+
+	elif [[ "$userInput" == "2" ]]; then
+		echo "Displaying only IPS:"
+		displayOnlyIPs
+	
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying only pages:"
+		displayOnlyPages
+
+	elif [[ "$userInput" == "4" ]]; then
+		echo "Histogram:"
+		histogram
+	
+	elif [[ "$userInput" == "5" ]]; then
+		echo "Frequent visitors:"
+		frequentVisitors
+
+	elif [[ "$userInput" == "6" ]]; then
+		echo "Suspicious visitors:"
+		suspiciousVisitors
+
+	else 
+		echo "I didn't understand that. Try again?"
+	fi
+done
+

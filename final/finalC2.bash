4549ccd613a9d191f1020ab10dbf73ccdf88e4d6
Lucien's ChampUser on xubuntu <lucien.aibel@mymail.champlain.edu>
2024-12-07 23:28:57 -0500

    Added a few more comments for safety.

--- final/finalC2.bash
+++ final/finalC2.bash
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#Check for correct arguments
 if [[ "${#}" != 2 ]]; then
 	echo "Invalid number of arguments."
 	echo "Correcy syntax: $0 [log file] [IOC file]"
 	exit 1
 fi
 
+#Get arguments
 logFile=$1
 IOCFile=$2
 
+#Read each line of the IOCFile, and for each, grep the logFile for that pattern
+#Cut at the end grabs the fields we want and sed removes the [ cut leaves dangling
 echo "$(cat $IOCFile)" | while read -r line;
 do
 	result="$(cat $logFile | grep "$line")"
--- final/finalC3.bash
+++ final/finalC3.bash
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+#Check argument number
 if [[ "${#}" != 1 ]]; then
 	echo "Invalid number of arguments."
 	echo "Correcy syntax: $0 [report file]"
 	exit 1
 fi
 
+#Grab arguments/constants
 reportFile=$1
 outputFile="/var/www/html/report.html"
 templateFile="template.html"
 
+#Read each line and format it into the table's data
 result="$(cat $reportFile | while read -r line;
 do
 	echo "<tr><td>$(echo $line | sed 's/ /<\/td><td>/g')</td></tr>"
 done)"
 
+#Escape the HTML slashes for feeding into sed
 result="$(echo $result | sed 's/\//\\\//g')"
 
+#Take the template and replace the placeholder with the reported data, then output it
+#The template has all the table headers and the rest of the page ready to go
 cat $templateFile | sed "s/<!--TABLE CONTENTS-->/$result/g" > $outputFile
+
+echo "Report converted to HTML and stored in $outputFile"
